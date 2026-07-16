cd835b458459b97ada5fa687ec56cf4bda9522e4
Ayoub <elhoudaiguiayoub2@gmail.com>
2026-07-16 10:26:23 +0100

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <header className="nav">
       <div className="nav-inner">
         <Link to="/" className="logo" onClick={close}>
-          EHYoub
+          AIGUI Store
         </Link>
 
         <nav className="nav-links desktop">
